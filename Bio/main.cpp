54a2fb153f3c660fed024119489d2905270db2c9
Ryan Schmitz <holyschmitz@ryan-schmitzs-imac.local>
2016-03-24 07:24:59 -0600

    Initial Commit

---
+++ Bio/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bio
+//
+//  Created by Ryan Schmitz on 2016-03-24.
+//  Copyright © 2016 Ryan Schmitz. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
